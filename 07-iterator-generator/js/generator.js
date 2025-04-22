50d70462af607d3f1b3d568a642ac7ac86d2f0da
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-22 10:35:43 +0700

    Update Iteration Generator

--- 07-iterator-generator/js/generator.js
+++ 07-iterator-generator/js/generator.js
@@ -1,8 +1,6 @@
-// JAVASCRIPT GENERATOR
-// Dalam JavaScript, fungsi reguler dijalankan berdasarkan model run-to-completion.
-// Ia tidak dapat berhenti di tengah jalan dan kemudian melanjutkan dari tempat ia berhenti
-// contoh
-
+// ==========================
+// 1. Fungsi biasa (tidak bisa pause)
+// ==========================
 function foo() {
   console.log("I");
   console.log("Cannot");
@@ -10,44 +8,58 @@ function foo() {
 }
 foo();
 
-function* genarate() {
-  console.log("invoke 1st time");
+// ==========================
+// 2. Generator function dasar
+// ==========================
+function* generate() {
+  console.log("Invoke 1st time");
   yield 1;
-  console.log("invoke 2nd time");
+
+  console.log("Invoke 2nd time");
   yield 2;
 }
 
-let gen = genarate();
-console.log(gen);
+let gen = generate();
+console.log("Generator object:", gen);
 
-let result2 = gen.next();
-console.log(result2);
+let result1 = gen.next(); // trigger 1st yield
+console.log("Hasil pertama:", result1); // { value: 1, done: false }
+
 for (const value of gen) {
-  console.log(value);
+  console.log("Iterasi berikutnya:", value); // Akan lanjut dari yield ke-2
 }
 
+// ==========================
+// 3. Generator tak hingga
+// ==========================
 function* forever() {
   let index = 0;
   while (true) {
     yield index++;
   }
 }
-let f = forever();
-console.log(f.next());
-console.log(f.next());
-console.log(f.next());
-console.log(f.next());
-console.log(f.next());
 
+let f = forever();
+console.log("Forever:", f.next()); // { value: 0, done: false }
+console.log("Forever:", f.next()); // { value: 1, done: false }
+console.log("Forever:", f.next()); // { value: 2, done: false }
+console.log("Forever:", f.next()); // { value: 3, done: false }
+console.log("Forever:", f.next()); // { value: 4, done: false }
+
+// ==========================
+// 4. Custom Iterator Class (tanpa generator)
+// ==========================
 class Sequence3 {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   [Symbol.iterator]() {
     let counter = 0;
     let nextIndex = this.start;
+
     return {
       next: () => {
         if (nextIndex <= this.end) {
@@ -63,16 +75,21 @@ class Sequence3 {
 }
 
 let oddNumber = new Sequence3(1, 10, 2);
+console.log("Custom iterator (tanpa generator):");
 for (const value of oddNumber) {
   console.log(value);
 }
 
+// ==========================
+// 5. Custom Iterator Class (dengan generator)
+// ==========================
 class Sequence4 {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   *[Symbol.iterator]() {
     for (let i = this.start; i <= this.end; i += this.interval) {
       yield i;
@@ -81,6 +98,7 @@ class Sequence4 {
 }
 
 let oddNumber2 = new Sequence4(1, 10, 2);
+console.log("Custom iterator (dengan generator):");
 for (const value of oddNumber2) {
   console.log(value);
 }
