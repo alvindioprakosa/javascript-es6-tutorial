bcbdc4b80f0ce2e08147b874da991c8e7f7f459a
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-22 10:36:51 +0700

    Update Dokumentation

--- 07-iterator-generator/js/script.js
+++ 07-iterator-generator/js/script.js
@@ -1,44 +1,55 @@
-// conoth perulangan array di ES5
+// ===========================================
+// 1. Perulangan array di ES5 dan ES6
+// ===========================================
 let remarks = ["A", "B", "C"];
+
+console.log("Loop dengan for ES5:");
 for (let i = 0; i < remarks.length; i++) {
   console.log(remarks[i]);
 }
 
+console.log("Loop dengan for...of ES6:");
 for (let remark of remarks) {
   console.log(remark);
 }
 
-// Contoh iterator ES6
+// ===========================================
+// 2. Contoh iterator ES6 (Sequence 1)
+// ===========================================
 class Sequence {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   [Symbol.iterator]() {
-    let counter = 0;
     let nextIndex = this.start;
     return {
       next: () => {
         if (nextIndex <= this.end) {
           let result = { value: nextIndex, done: false };
           nextIndex += this.interval;
-          counter++;
           return result;
         }
-        return { value: counter, done: true };
+        return { value: undefined, done: true };
       },
     };
   }
 }
 
-let eventNumber = new Sequence(2, 10, 2);
-for (const num of eventNumber) {
+let evenNumbers = new Sequence(2, 10, 2);
+
+console.log("Loop dengan for...of:");
+for (const num of evenNumbers) {
   console.log(num);
 }
 
-// mengacess Symbol.iterable secara explicit
-let iterator = eventNumber[Symbol.iterator]();
+// ===========================================
+// 3. Akses Symbol.iterator secara eksplisit
+// ===========================================
+console.log("Akses Symbol.iterator manual:");
+let iterator = evenNumbers[Symbol.iterator]();
 let result = iterator.next();
 
 while (!result.done) {
@@ -46,24 +57,27 @@ while (!result.done) {
   result = iterator.next();
 }
 
+// ===========================================
+// 4. Sequence2 dengan implementasi return()
+//    untuk cleanup saat loop di-break
+// ===========================================
 class Sequence2 {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   [Symbol.iterator]() {
-    let counter = 0;
     let nextIndex = this.start;
     return {
       next: () => {
         if (nextIndex <= this.end) {
           let result = { value: nextIndex, done: false };
           nextIndex += this.interval;
-          counter++;
           return result;
         }
-        return { value: counter, done: true };
+        return { value: undefined, done: true };
       },
       return: () => {
         console.log("Clean up ...................");
@@ -73,10 +87,11 @@ class Sequence2 {
   }
 }
 
-let oddNumber = new Sequence2(1, 10, 2);
-for (const num of oddNumber) {
+console.log("Loop dengan break (testing return):");
+let oddNumbers = new Sequence2(1, 10, 2);
+for (const num of oddNumbers) {
   if (num > 7) {
-    break;
+    break; // trigger return()
   }
   console.log(num);
 }
